ed27d5cf395c24df43d74e8682c62d17349f22df
ivan <sh.on.98@mail.ru>
2025-05-30 20:19:04 +0500

    Update 1.js

--- восьмая тема/1.js
+++ восьмая тема/1.js
@@ -1,19 +1,14 @@
-// Функция для подсчёта гласных букв в слове
 const countVowels = (word) => {
-    // Определяем массив гласных букв
+
     const vowels = ['a', 'e', 'i', 'o', 'u'];
   
-    // Приводим слово к нижнему регистру и разбиваем на массив символов
     const characters = word.toLowerCase().split('');
-  
-    // Используем filter() для фильтрации только гласных букв
+
     const vowelCount = characters.filter(char => vowels.includes(char)).length;
-  
-    // Возвращаем количество гласных букв
+
     return vowelCount;
   };
   
-  // Пример использования
   const word = 'JavaScript';
   const vowelCount = countVowels(word);
-  console.log(vowelCount); // Выведет 3
+  console.log(vowelCount);
