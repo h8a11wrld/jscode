b54d4f3bdd06ed98382ab6a4e2c5b88815ff234d
ivan <sh.on.98@mail.ru>
2025-05-30 21:01:08 +0500

    Add files via upload

---
+++ задание3/index.js
@@ -0,0 +1,28 @@
+// Сделайте код программы короче, используя инкремент.
+
+let index = 0 // Счетчик
+
+// Бугун 1
+let runner1 = "Денис"
+index = index + 1
+console.log(index, runner1);
+
+// Бугун 2
+let runner2 = "Виктория"
+index = index + 1
+console.log(index, runner2);
+
+// Бугун 3
+let runner3 = "Антон"
+index = index + 1
+console.log(index, runner3);
+
+// Бугун 4
+let runner4 = "Лена"
+index = index + 1
+console.log(index, runner4);
+
+// Бугун 5
+let runner5 = "Никита"
+index = index + 1
+console.log(index, runner5);
