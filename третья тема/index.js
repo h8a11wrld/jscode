f29b492e44ec0024dc01610e74ea7c63ead11798
ivan <sh.on.98@mail.ru>
2025-05-30 21:05:03 +0500

    Update index.js

--- третья тема/index.js
+++ третья тема/index.js
@@ -1,13 +1,11 @@
-// Программа для работы банкомата
-
-let cardInserted = true; // Состояние карты: true — карта вставлена, false — карта отсутствует
-let availableBalance = 500; // Баланс на карте
+let cardInserted = true; 
+let availableBalance = 500; 
 
 const operationAmount = parseFloat(prompt("Введите сумму операции:"));
 
 if (cardInserted && operationAmount <= availableBalance) {
   console.log("Операция выполняется");
-  availableBalance -= operationAmount; // Обновление баланса после успешной операции
+  availableBalance -= operationAmount; 
 } else {
   console.log("Операция отклонена");
 }
