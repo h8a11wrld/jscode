99e6fcdd962e4700d0106f57113af298998bed0e
ivan <sh.on.98@mail.ru>
2025-05-30 20:16:10 +0500

    Update task4.js

---
+++ четвертая тема/task4.js
@@ -0,0 +1,11 @@
+let count = 3;
+
+// Функция для увеличения count на 1
+function countPlusOne() {
+    count += 1;
+    console.log(count);
+}
+
+// Пример вызова:
+countPlusOne();  // Вывод: 4
+countPlusOne();  // Вывод: 5
